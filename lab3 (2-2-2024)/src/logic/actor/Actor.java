f123ce145b121fd504993123235f7ff9f83cc102
tin2003tin <152775124+tin2003tin@users.noreply.github.com>
2024-03-14 12:57:08 +0700

    save

--- lab3 (2-2-2024)/src/logic/actor/Actor.java
+++
@@ -1,6 +0,0 @@
-package logic.actor;
-
-public abstract class Actor{
-    public abstract int getLevel();
-    public abstract void attack();
-}
--- lab3 (2-2-2024)/src/logic/item/Item.java
+++
@@ -1,6 +0,0 @@
-package logic.item;
-
-public abstract class Item {
-    public abstract int getLevel();
-    public  abstract void effect();
-}
